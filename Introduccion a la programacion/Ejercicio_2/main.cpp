f9a07aa27a64c55f228e973bb56ca08e7c833a31
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-05-19 21:47:13 -0300

    se agrega ejercicio 2 de la semana 3

---
+++ Introduccion a la programacion/Ejercicio_2/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	//ejercicio 2:mostrar nombre, salto de linea, apellido
+	cout << "Lucas Agustin";
+	cout << endl;
+	cout << "Morales Romero";
+	return 0;
+}
+

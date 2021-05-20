cd154430d4a28f99248a492da1f7e0af899455a3
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-05-19 22:26:50 -0300

    se agrea ejercicio 5

---
+++ Introduccion a la programacion/Ejercicio_5/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	//ejercicio 5 mostrar nombre apellido edad pero con interfaz utilizando variables
+	int edad = 28;
+	string nombre = "Lucas Agustin";
+	string apellido = "Morales Romero";
+	string linea = "=========================";
+	cout << linea << endl;
+	cout << "Mi nombre es: " << nombre << endl;
+	cout << "Mi apellido es: " << apellido << endl;
+	cout << "Tengo " << edad << " anios" << endl;
+	cout << linea << endl;
+	
+	return 0;
+}
+

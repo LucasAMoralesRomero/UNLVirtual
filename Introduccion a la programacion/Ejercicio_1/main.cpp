17da97d0670df1c56b61b5a7146ccfb960e5eb29
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-05-19 21:37:47 -0300

    init

---
+++ Introduccion a la programacion/Ejercicio_1/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	//tarea 1 mostrar tu nombre y apellido en pantalla
+	cout << "Lucas Agustin Morales Romero";
+	return 0;
+}
+

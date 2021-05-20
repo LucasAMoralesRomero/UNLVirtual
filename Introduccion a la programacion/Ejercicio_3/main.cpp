a0c60024ccd1641fc2fed82bd53c8843cccf2dfe
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-05-19 21:53:12 -0300

    se agrega ejercicio 3

---
+++ Introduccion a la programacion/Ejercicio_3/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	// ejercicio 3: mostrar nombre apellido y edad
+	cout<<"Soy Lucas Agustin Morales Romero";
+	cout<<endl;
+	cout<<"Tengo 28 anios";
+	return 0;
+}
+

670e5835e9f8a4e607822104259af887b10548b1
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-05-25 08:27:23 -0300

    se continua con el ejercicio 14

--- Introduccion a la programacion/Ejercicio_14/main.cpp
+++ Introduccion a la programacion/Ejercicio_14/main.cpp
@@ -3,18 +3,28 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
 	string nombre = "";
-	int vida = 10;
-	int runa1 = 0;
-	int runa2 = 0;
-	int runa3 = 0;
+	double vida = 100;
+	double runa1 = 0;
+	double runa2 = 0;
+	double runa3 = 0;
+	double total = 0;
 	string barra = "=========================";
+	bool estaVivo;
 	cout << barra << endl;
 	cout << "Ingrese nombre del personaje: ";
 	cin >> nombre;
 	cout << endl;
 	cout << "Su nombre es: " << nombre << endl;
 	cout << "Sus puntos de vida son: " << vida << endl;
-	
+	cout << "ngresar primera runa: ";
+	cin >> runa1;
+	cout << endl;
+	cout << "Ingrese segunda runa: ";
+	cin >> runa2;
+	cout << endl;
+	cout << "Ingrese tercer runa: ";
+	cin >> runa3;
+	cout <<"El personaje sigue vivo?: " << estaVivo;
 	return 0;
 }
 

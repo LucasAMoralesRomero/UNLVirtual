09514c4051fef75bfe57bf9d5566f1b4c6583e10
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2021-06-11 19:57:35 -0300

    se agrega ejercicio 8

--- Introduccion a la programacion/Ejercicio_8/main.cpp
+++ Introduccion a la programacion/Ejercicio_8/main.cpp
@@ -2,22 +2,33 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	//ejercicio 8: sin usar un fot, hacer un programa que cuente
+	//ejercicio 8: sin usar un for, hacer un programa que cuente
 	//de 1 a 10 en una variable y salga
 	
-	:agregar1
-	{
-		cout << " " << numero << " "<<;
-		numero ++;
-		goto :volver;
-	};
+	
 	int numero = 1;
 	string linea = "=========================";
 	cout << linea << endl;
-	:volver;
-	if (numero < 10) goto :agregar1;
-	cout << endl;
-	cout << linea << endl;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " ";
+	numero = numero + 1;
+	cout << numero << " " << endl;
+	cout << linea;
 	return 0;
 }
 	
---
+++ Introduccion a la programacion/Ejercicio_9/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+//actividad 9: ingresar un numero e informar cual ingrese
+	int Numero_Ingresado = 0;
+	cout << "Ingrese un numero entero, por favor" <<endl;
+	
+	return 0;
+}
+
